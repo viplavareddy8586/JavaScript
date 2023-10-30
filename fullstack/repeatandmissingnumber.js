f257eea5fe6d9f71f4d06e5cd913a85874b26629
viplavareddy8586 <146053562+viplavareddy8586@users.noreply.github.com>
2023-10-30 15:56:57 +0530

    Update repeatandmissingnumber.js

--- fullstack/repeatandmissingnumber.js
+++ fullstack/repeatandmissingnumber.js
@@ -1,21 +1,32 @@
-let a=[3,1,2,5,6,4,8]
-let t
+let a=[4,3,6,5,7,9,2,1,1]
+let repeatednumber
+let min=a[0]
+let max=a[0]
+let sum=0
+let tsum=0
+let missingnumber=0
 for(let i=0;i<a.length;i++){
 
-    for(let j=i+1;j<a.length;j++){
-        if(a[i]>a[j]){
-        t=a[i]
-        a[i]=a[j]
-        a[j]=t
+        if(a[i]>max){
+            max=a[i]
+        }
+        if(a[i]<min){
+            min=a[i]
+        }
+        sum=sum+a[i]
+        for(let k=0;k<a.length;k++){
+            if(a[i]==a[k]){
+           repeatednumber=a[i]
+            }
         }
     }
-}
-console.log(a)
-for(let k=0;k<a.length;k++){
-    
-    if(a[k]!=k+1){
-        console.log("missingnum--->"+(k+1))
-        break
+    for(let j=min;j<=max;j++){
+    tsum=tsum+j
     }
-}
+    // console.log("-------"+tsum)
+    // console.log(sum)
+    
+    missingnumber=tsum-(sum-repeatednumber)
+    console.log(missingnumber)
+    console.log(repeatednumber)
 
